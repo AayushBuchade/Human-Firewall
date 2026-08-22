c8c4e56f55b8406e2d0511196230bc3c21f46e30
AayushBuchade <aayush.buchade@gmail.com>
2026-08-22 20:22:16 +0530

    Connect frontend to Railway backend

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'https://NEW-BACKEND-DOMAIN/api' });
+const api = axios.create({ baseURL: 'https://human-firewall-production.up.railway.app/api' });
 
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('hf_token');
